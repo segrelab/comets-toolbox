51675a23fb3fb1fc9ee5ce9ffc463ff677f450d6
Uday Tripathi <udayt@bu.edu>
2016-08-05 17:13:05 -0400

    Giant Hash Table Creator

--- genetic_algorithm/runner.m
+++ genetic_algorithm/runner.m
@@ -1,9 +1,12 @@
-for i=1:15
+masterHash=containers.Map;
+for i=1:50
 main=Main();
 main=main.initModels(models);
 main=main.initMets(mets);
-main=main.initGenomes(20,3,3); 
+main=main.initGenomes(20,3,3);
 main.hashMap=masterHash;
-main=main.run(10,10,newMets,newModels,5,'Cobra','EX Nitrite e0');
+rand1=randi([3,10]);
+rand2=20-rand1-randi([1,9]);
+main=main.run(50,rand1,newMets,newModels,rand2,'Cobra','EX Nitrite e0');
 masterHash=main.hashMap;
 end
