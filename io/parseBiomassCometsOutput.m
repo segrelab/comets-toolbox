dbe0205f672fb937252de22789b19938e32354f5
jgoldford <joshuagoldford@vpn1-dhcp65-16.bu.edu>
2016-11-16 18:43:31 -0500

    added parse biomass function

---
+++ io/parseBiomassCometsOutput.m
@@ -0,0 +1,42 @@
+function [out] = parseBiomassCometsOutput(biomassFile)
+% parseBiomassCometsOutput:
+% parses biomass file into struct with following fields:
+% x: x pixel
+% y: y pixel
+% biomass: biomass value
+% timeStep: cycle time step
+% model: model ID
+
+f = fopen(biomassFile);
+tline = fgetl(f);
+% build a 
+k = 1;
+while ischar(tline)
+    
+        timeStep = str2num(regexp(tline,'(?<=biomass\_)\d*','match','ONCE'));
+        modelID = str2num(regexp(tline,'(?<=biomass\_\d*\_)\d*','match','ONCE'));
+    
+        if isempty(regexp(tline,'sparse','match','ONCE'));
+            x = str2num(regexp(tline,'(?<=biomass\_\d*\_\d*\()\d*','match','ONCE'));
+            y = str2num(regexp(tline,'(?<=biomass\_\d*\_\d*\(\d*, )\d*','match','ONCE'));
+            z = str2num(tline(regexp(tline,'(?<=\= )\d'):end-1));
+            
+            X(k) = x;
+            Y(k) = y;
+            Biomass(k) = z;
+            T(k) = timeStep;
+            ModelId(k) = modelID;
+            k = k+1;
+        end
+        tline = fgetl(f);
+
+    
+end
+
+out.x = X;
+out.y = Y;
+out.biomass = Biomass;
+out.model = ModelId;
+out.timeStep = T;
+
+end
