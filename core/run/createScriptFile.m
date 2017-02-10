91d19e35d2c58abc1c42be206e8fcebb781acf4f
mquintin <mquintin@bu.edu>
2017-02-10 13:42:40 -0500

    reorganize createScriptFile() so params files override params in the layout, add check/arg so see params files exist

--- core/run/createScriptFile.m
+++ core/run/createScriptFile.m
@@ -1,26 +1,46 @@
-function createScriptFile( directory, layoutName )
-%CREATESCRIPTFILE Create the comets_script.txt file which tells
-%command-line COMETS where to find the files it needs to run
+function createScriptFile( directory, layoutName, paramFilesExist )
+%CREATESCRIPTFILE(directory, [layoutName], [paramFilesExist]) Create the
+%comets_script.txt file which tells command-line COMETS where to find the
+%files it needs to run
 %
 %Layout name will be guessed if not provided.
+%If paramFilesExist is not given, the function will check the given
+%directory for 'global_params.txt' and 'package_params.txt'.
+%If paramFilesExist = false, the script will exclude any params files even
+%if they really do exist
 
 if nargin < 2
     layoutName = 'comets_layout.txt';
 elseif ~strfind(layoutName,'.') %the layout name doesn't have an extension
-    layoutName = [layoutName '.txt'];    
+    layoutName = [layoutName '.txt'];
 end
 
 scriptfile = fullfile(directory, 'comets_script.txt');
+lfile = fullfile(directory,layoutName);
 gpfile = fullfile(directory,'global_params.txt');
 ppfile = fullfile(directory,'package_params.txt');
-lfile = fullfile(directory,layoutName);
+
+%do the param files exist?
+gpexist = exist(gpfile,'file');
+ppexist = exist(ppfile,'file');
+
+%default behavior if paramFilesExist is not given
+if nargin < 3
+    paramFilesExist = gpexist | ppexist;
+end
 
 file = fopen(scriptfile,'w');
 
-fprintf(file,'load_comets_parameters %s\n',gpfile);
-fprintf(file,'load_package_parameters %s\n',ppfile);
 fprintf(file,'load_layout %s\n',lfile);
+
+if paramFilesExist
+    if gpexist
+        fprintf(file,'load_comets_parameters %s\n',gpfile);
+    end
+    if ppexist
+        fprintf(file,'load_package_parameters %s\n',ppfile);
+    end
+end
 
 fclose(file);
 end
-
