01ebadad10d5dbdfbb28c6a815d88ea505c45f49
mquintin <mquintin@bu.edu>
2017-02-10 13:33:10 -0500

    add separateParamsFiles argument to createCometsFiles()

--- core/run/createCometsFiles.m
+++ core/run/createCometsFiles.m
@@ -1,14 +1,33 @@
-function [ output_args ] = createCometsFiles(layout, directory)
-%CREATECOMETSFILES %write the CometsScript, CometsParams, and PackageParams files necessary
+function [ output_args ] = createCometsFiles(layout, directory, layoutFileName, separateParamsFiles)
+%CREATECOMETSFILES(layout, [directory], [layoutFileName], [separateParamsFiles])
+%write the comets_script, global_params, and package_params files necessary
 %for a COMETS run based on the given CometsLayout
+%Args:
+% layout: a CometsLayout object
+% directory: location to write to (defaults to current directory)
+% layoutFileName: name and extension of the layout file (default
+% 'comets_layout.txt')
+% separateParamsFiles: If true, creates separate files to contain the
+% global and package parameters. If false, all parameters are included in
+% the body of the layout file. (default false)
 
-%TODO: add argument to specify name of layout file
+if nargin < 2
+    directory = pwd;
+end
+if nargin < 3
+    layoutFileName = 'comets_layout.txt';
+end
+if nargin < 4
+    separateParamsFiles = false;
+end
 
-createScriptFile(directory, 'comets_layout.txt');
-createCometsParamsFile(directory, 'global_params.txt', layout.params);
-createPackageParamsFile(directory, 'package_params.txt', layout.params);
+if separateParamsFiles
+    createCometsParamsFile(directory, 'global_params.txt', layout.params);
+    createPackageParamsFile(directory, 'package_params.txt', layout.params);
+end
+createScriptFile(directory, layoutFileName, separateParamsFiles);
 
-writeCometsLayout(layout, directory, 'comets_layout.txt'); 
+writeCometsLayout(layout, directory, 'comets_layout.txt', ~separateParamsFiles, true); 
     %writeCometsLayout also creates model files
 
 end
