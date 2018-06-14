4c09e4b4981883e84d27a8792619a1ee8bde8eb9
mquintin <mquintin@bu.edu>
2018-06-14 15:29:18 -0400

    restored M2HTML documentation
    It's not worth reformatting comments to be Sphinx-compliant at this time

--- docs/_build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to COMETS Toolbox\u2019s documentation!"],titleterms:{comet:0,document:0,indic:0,tabl:0,toolbox:0,welcom:0}})
