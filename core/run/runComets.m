27512efb543a8fc7deb844e0740eed203e055a49
mquintin <mquintin@bu.edu>
2019-03-14 16:14:42 -0400

    have runComets delete logs with name conflicts before executing

--- core/run/runComets.m
+++ core/run/runComets.m
@@ -13,6 +13,30 @@ cd(directory);
 
 createCometsFiles(layout,pwd);%create layout,model,script & param files
 
+%clear out files with name conflicts for the output files
+%this should help avoid issues where a silently failed run results in 
+%scripts loading old results
+logfilenames = {};
+if layout.params.writeMediaLog
+    logfilenames = [logfilenames layout.params.mediaLogName];
+end
+if layout.params.writeBiomassLog
+    logfilenames = [logfilenames layout.params.biomassLogName];
+end
+if layout.params.writeTotalBiomassLog
+    logfilenames = [logfilenames layout.params.totalBiomassLogName];
+end
+if layout.params.writeFluxLog
+    logfilenames = [logfilenames layout.params.fluxLogName];
+end
+for i = 1:length(logfilenames)
+    n = logfilenames{i};
+    if exist(n,'file')
+        disp(['Log file ' n ' already exists! Deleting it before COMETS execution...']);
+        delete(n);
+    end
+end     
+
 disp(['Executing COMETS in the directory ' pwd]);
 cometsOutput = runCometsOnDirectory(pwd);%run
 
