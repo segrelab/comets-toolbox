51d1c0636981eb2c4ded360fe9b1b49194394ca3
mquintin <mquintin@bu.edu>
2020-07-17 16:23:33 -0400

    make runCometsOnDirectory more friendly to alternative command lines
    don't use system variable calls in the command line call, since some systems expect eg %VARNAME% and others $VARNAME

--- core/run/runCometsOnDirectory.m
+++ core/run/runCometsOnDirectory.m
@@ -20,15 +20,19 @@ function comets_output = runCometsOnDirectory(run_COMETS_folder)
 %   -If no copy of comets_w64_scr.bat can be found, create a new one.
 %   -If multiple versions of comets*.jar exist, use the most recent one
 %
-% TODO: Update this to not rely on .bat files, so it works on Linux
+% TODO: Update this to not require Gurobi
 
 %% Add COMETS Java Classpath to MATLAB
 cometshome = getenv('COMETS_HOME');
 if isempty(cometshome)
-   error('COMETS_HOME environmental variable not set')
+   error('COMETS_HOME environmental variable is not set')
+end
+gurobihome = getenv('GUROBI_HOME');
+if isempty(gurobihome)
+   error('GUROBI_HOME environmental variable is not set')
 end
 javaclasspath(cometshome); % add COMETS classpath to MATLAB
-    
+javaclasspath(gurobihome);    
 
 %% Set up Working Directory with COMETS Files
 tempscript = 0; %we delete the script after running if it's copied from COMETS_HOME
@@ -51,11 +55,10 @@ if exist([run_COMETS_folder filesep execfile],'file') ~= 2 % script bat file not
             %get the name of the most recently created COMETS jar file in COMETS_HOME
             jars = [dir([cometshome filesep 'comets*.jar']); dir([cometshome filesep 'bin' filesep 'comets*.jar'])];
             [tmp, idx] = sort(datetime({jars.date}));
-            jarname = jars(idx(length(idx))).name;
-                
+            jarname = jars(idx(length(idx))).name; 
             fileid = fopen([run_COMETS_folder filesep execfile],'w');
             %fprintf(fileid,'%s%s%s',['java -Xmx2048m -classpath ' cometshome '//'],jarname,[';C:/Users/mquintin/workspace/lib/jmatio.jar;' cometshome '/lib/jmatio.jar;' cometshome '/lib/x64/glpk-java.jar;' cometshome '/lib/jogamp-all-platforms/jar/jogl-all.jar;' cometshome '/lib/jogamp-all-platforms/jar/gluegen.jar;' cometshome '/lib/jogamp-all-platforms/jar/gluegen-rt.jar;' cometshome '/lib/jogamp-all-platforms/jar/gluegen-rt-natives-windows-amd64.jar;' cometshome '/lib/jogamp-all-platforms/jar/jogl-all-natives-windows-amd64.jar;%GUROBI_HOME%/lib/gurobi.jar -Djava.library.path=' cometshome '/lib;' cometshome '/lib/x64;%GUROBI_HOME%;%GUROBI_HOME%/lib/;%GUROBI_HOME%/bin edu.bu.segrelab.comets.Comets -loader edu.bu.segrelab.comets.fba.FBACometsLoader -script comets_script.txt']);
-            execstr = ['java -Xmx2048m -classpath %COMETS_HOME%/' jarname ';%COMETS_HOME%/bin/' jarname ';%COMETS_HOME%/lib/glpk-java.jar;%COMETS_HOME%/lib/jogamp-all-platforms/jar/jogl-all.jar;%COMETS_HOME%/lib/jogamp-all-platforms/jar/gluegen.jar;%COMETS_HOME%/lib/jogamp-all-platforms/jar/gluegen-rt.jar;%COMETS_HOME%/lib/jogamp-all-platforms/jar/gluegen-rt-natives-windows-amd64.jar;%COMETS_HOME%/lib/jogamp-all-platforms/jar/jogl-all-natives-windows-amd64.jar;%COMETS_HOME%/lib/jmatio.jar;%COMETS_HOME%/lib/jamtio.jar;%GUROBI_HOME%/lib/gurobi.jar -Djava.library.path=%COMETS_HOME%/;%COMETS_HOME%/lib/;%GUROBI_HOME%/lib/;%GUROBI_HOME%/bin edu.bu.segrelab.comets.Comets -loader edu.bu.segrelab.comets.fba.FBACometsLoader -script comets_script.txt'];
+            execstr = ['java -Xmx2048m -classpath ' cometshome '/' jarname ';' cometshome '/bin/' jarname ';' cometshome '/lib/glpk-java.jar;' cometshome '/lib/jogamp-all-platforms/jar/jogl-all.jar;' cometshome '/lib/jogamp-all-platforms/jar/gluegen.jar;' cometshome '/lib/jogamp-all-platforms/jar/gluegen-rt.jar;' cometshome '/lib/jogamp-all-platforms/jar/gluegen-rt-natives-windows-amd64.jar;' cometshome '/lib/jogamp-all-platforms/jar/jogl-all-natives-windows-amd64.jar;' cometshome '/lib/jmatio.jar;' cometshome '/lib/jamtio.jar;' gurobihome '/lib/gurobi.jar -Djava.library.path=' cometshome '/;' cometshome '/lib/;' gurobihome '/lib/;' gurobihome '/bin edu.bu.segrelab.comets.Comets -loader edu.bu.segrelab.comets.fba.FBACometsLoader -script comets_script.txt'];
             fprintf(fileid,'%s%s%s',execstr);
             fclose(fileid);
         end
